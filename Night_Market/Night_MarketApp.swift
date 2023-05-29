28c9ff32189836167f9c4ab5284e789bcd9dabcd
Wassim Zerouta <wassim.zerouta@gmail.com>
2023-05-29 20:19:04 +0200

    Initial Commit

---
+++ Night_Market/Night_MarketApp.swift
@@ -0,0 +1,17 @@
+//
+//  Night_MarketApp.swift
+//  Night_Market
+//
+//  Created by Wass on 29/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct Night_MarketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
