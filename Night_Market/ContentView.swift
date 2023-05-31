9b39741db992676b5a5808ba3c61f9efe507a692
Wassim Zerouta <wassim.zerouta@gmail.com>
2023-05-31 23:24:45 +0200

    Get user's position

--- Night_Market/ContentView.swift
+++ Night_Market/ContentView.swift
@@ -6,23 +6,52 @@
 //
 
 import SwiftUI
+import Combine
 
 struct ContentView: View {
+    
+    @StateObject var deviceLocationServices = DeviceLocationService.shared
+    
+    @State var tokens: Set<AnyCancellable> = []
+    @State var coordinates: (lat: Double, lon: Double) = (0,0)
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("lat: \(coordinates.lat)")
+            Text("lon: \(coordinates.lon)")
         }
-        .padding()
+        .onAppear{
+            observeCoordinatorUpdates()
+            observedLocationAccessDenied()
+            deviceLocationServices.requestLocationUpdates()
+        }
+    }
+        
+    
+    
+    func observeCoordinatorUpdates() {
+            deviceLocationServices.coordinatesPublisher
+                .receive(on: DispatchQueue.main)
+                .sink { completion in
+                    if case .failure(let error) = completion { print(error) }
+                    
+                } receiveValue: { coordinates in
+                    self.coordinates = (coordinates.latitude, coordinates.longitude)
+                }
+                .store(in: &tokens)
+        }
+    
+    func observedLocationAccessDenied() {
+        deviceLocationServices.deniedLocationAccessPublisher
+            .receive(on: DispatchQueue.main)
+            .sink { print("Position refusé")
+            }
+            .store(in: &tokens)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        Group {
             ContentView()
         }
-    }
 }
--- Night_Market/ViewModels/DeviceLocationService.swift
+++ Night_Market/ViewModels/DeviceLocationService.swift
@@ -6,3 +6,53 @@
 //
 
 import Foundation
+import CoreLocation
+import Combine
+
+class DeviceLocationService: NSObject, CLLocationManagerDelegate, ObservableObject {
+    var coordinatesPublisher = PassthroughSubject<CLLocationCoordinate2D, Error>()
+    var deniedLocationAccessPublisher = PassthroughSubject<Void, Never>()
+    
+    private override init() {
+        super.init()
+    }
+    
+    static let shared = DeviceLocationService()
+    
+    func requestLocationUpdates() {
+        switch locationManager.authorizationStatus {
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        case .authorizedWhenInUse, .authorizedAlways:
+            locationManager.startUpdatingLocation()
+        default:
+            deniedLocationAccessPublisher.send()
+        }
+    }
+    
+    private lazy var locationManager: CLLocationManager = {
+        let manager = CLLocationManager()
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.delegate = self
+        return manager
+    }()
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        switch locationManager.authorizationStatus {
+        case .authorizedWhenInUse, .authorizedAlways:
+            manager.startUpdatingLocation()
+        default:
+            manager.stopUpdatingLocation()
+            deniedLocationAccessPublisher.send()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else { return }
+        coordinatesPublisher.send(location.coordinate)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        coordinatesPublisher.send(completion: .failure(error))
+    }
+}
